a404d7627fbed037799b4e261fe05a5865edccb8
unknown <yosefsitumorang97@gmail.com>
2021-06-19 01:50:21 +0700

    create auth, product reducer

---
+++ src/redux/reducer/auth.js
@@ -0,0 +1,43 @@
+const initialState = {
+  token: null,
+  user: null,
+  message: '',
+  errorMsg: ''
+}
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SIGN_UP': {
+      return {
+        ...state,
+        message: action.payload
+      }
+    }
+    case 'SIGN_IN': {
+      return {
+        ...state,
+        token: action.payload,
+        user: action.user
+      }
+    }
+    case 'SET_AUTH_MESSAGE': {
+      return {
+        ...state,
+        message: '',
+        errorMsg: action.payload
+      }
+    }
+    case 'SIGNOUT': {
+      return {
+        ...state,
+        token: null,
+        message: '',
+        errorMsg: ''
+      }
+    }
+    default:
+      return { ...state }
+  }
+}
+
+export default authReducer
