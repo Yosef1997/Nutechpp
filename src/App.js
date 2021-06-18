260357d1ad0eb13abc64c96f6c632a60011846ed
unknown <yosefsitumorang97@gmail.com>
2021-06-19 01:04:38 +0700

    create navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
-import persistedStore from './Redux/store'
+import persistedStore from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import Home from './pages/home'
@@ -10,21 +10,23 @@ import Detail from './pages/detail'
 import Input from './pages/input'
 
 
-function App() {
-  const {store, persistor} = persistedStore()
-  return (
-<Provider store = {store}>
-  <PersistGate persistor = {persistor}>
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/detail' component={Detail} />
-        <Route path='/product' component={Input} />
-      </Switch>
-    </BrowserRouter>
-  </PersistGate>
-</Provider>
-  )
+class App extends Component {
+  render () {
+    const { store, persistor } = persistedStore()
+    return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/detail' component={Detail} />
+            <Route path='/product' component={Input} />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    )
+  }
 }
 
 export default App
