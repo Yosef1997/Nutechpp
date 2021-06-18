11fa747996837fb39cd49104447fbccbe63c17bf
unknown <yosefsitumorang97@gmail.com>
2021-06-19 00:55:07 +0700

    create navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import persistedStore from './Redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import Home from './pages/home'
 import Detail from './pages/detail'
 import Input from './pages/input'
 
 
 function App() {
+  const {store, persistor} = persistedStore()
   return (
-    <div>
-      
-    </div>
+<Provider store = {store}>
+  <PersistGate persistor = {persistor}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/detail' component={Detail} />
+        <Route path='/product' component={Input} />
+      </Switch>
+    </BrowserRouter>
+  </PersistGate>
+</Provider>
   )
 }
 
